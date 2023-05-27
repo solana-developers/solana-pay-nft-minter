4754328f1727c16fbf97807de0f0ddbfacf9febd
John <johnzhengyuliu@gmail.com>
2023-05-27 00:59:11 -0500

    reference

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react"
+import WalletContextProvider from "../contexts/WalletContextProvider"
+import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <WalletContextProvider>
+        <Component {...pageProps} />
+      </WalletContextProvider>
+    </ChakraProvider>
+  )
 }
