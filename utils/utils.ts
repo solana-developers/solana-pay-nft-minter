f1595569879f037bceb9fbb3991ced78037d8eb5
John <johnzhengyuliu@gmail.com>
2023-08-25 14:32:28 -0500

    update metadata uris

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,17 +1,17 @@
 // Off-chain metadata
 const uris = [
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/Climate/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/ClosedCube/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/CompressedCoil/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/CompressedNFT/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/Consensus/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/DeveloperPortal/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/DeveloperToolkit/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/GlobalPayments/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/OpenCube/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/ParallelTransactions/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/SagaPhone/metadata.json",
-  "https://raw.githubusercontent.com/ZYJLiu/opos-asset/main/assets/Security/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/Climate/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/ClosedCube/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/CompressedCoil/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/CompressedNFT/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/Consensus/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/DeveloperPortal/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/DeveloperToolkit/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/GlobalPayments/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/OpenCube/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/ParallelTransactions/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/SagaPhone/metadata.json",
+  "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/Security/metadata.json",
 ];
 
 // Select a random URI from the list
