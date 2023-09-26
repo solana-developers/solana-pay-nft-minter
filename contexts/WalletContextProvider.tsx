707fa0872bcbb443101836eb9711ebd6d3faa103
John Liu <johnzhengyuliu@gmail.com>
2023-09-26 01:12:02 -0500

    remove unnecessary wallet adapters

--- contexts/WalletContextProvider.tsx
+++ contexts/WalletContextProvider.tsx
@@ -5,11 +5,7 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  BackpackWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
@@ -17,14 +13,7 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets = useMemo(
-    () => [
-      new BackpackWalletAdapter(),
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-    ],
-    [network]
-  );
+  const wallets = useMemo(() => [new SolflareWalletAdapter()], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
